cc5de4f8fc01f3cd4a996f1fa054d54263e6ea61
Diya Lohani <shellylohani2018@gmail.com>
2024-06-29 18:40:56 +0530

    modified Header.jsx--made reponsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../assets/Images/logo.png'
 import user from './../assets/Images/user.png'
 import {
@@ -7,11 +7,13 @@ import {
     HiStar,
     HiPlayCircle,
     HiTv,
-    HiPlus
+    HiPlus,
 } from 'react-icons/hi2'
+import { HiDotsVertical } from "react-icons/hi"
 import HeaderItem from './HeaderItem'
 
 function Header() {
+    const [toggle, setToggle] = useState(false);
 
     const menu = [
         {
@@ -44,9 +46,30 @@ function Header() {
     <div className='flex items-center gap-8 justify-between p-5'>
         <div className='flex items-center gap-8'>
             <img src = {logo} className='w-[80px] md:w-[100px] object-cover'/>
-            {menu.map((item) => (
+
+            <div className='hidden md:flex gap-8'>
+                {menu.map((item) => (
                 <HeaderItem name = {item.name} Icon = {item.icon} />
-            ))}
+                ))}
+            </div>
+            
+            <div className='flex md:hidden gap-5'>
+                {menu.map((item, index) => index<3 &&(
+                <HeaderItem name = {''} Icon = {item.icon} />
+                ))}
+            
+                <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+                    <HeaderItem name={''} Icon={HiDotsVertical} />
+                    
+                    {toggle ? 
+                    <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+                        {menu.map((item, index) => index>2 &&(
+                        <HeaderItem name = {item.name} Icon = {item.icon} />
+                        ))} 
+                    </div>
+                    : null}
+                </div>
+            </div>
         </div>
         <img src={user} className='w-[55px] rounded-full'/>
     </div>
