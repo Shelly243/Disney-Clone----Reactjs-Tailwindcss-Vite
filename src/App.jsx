d641221969d440ca7ee613894e295d46e7ca5f8c
Diya Lohani <shellylohani2018@gmail.com>
2024-07-01 17:54:00 +0530

    create slider.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
+import Slider from './Components/Slider'
 
 function App() {
   return (
     <div className="bg-black h-screen">
       <Header/>
+      <Slider/>
     </div>
   )
 }
