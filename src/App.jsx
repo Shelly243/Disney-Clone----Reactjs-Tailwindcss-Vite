01a5024a11d87ff9e36ec91dc4a1024026f446f3
Diya Lohani <shellylohani2018@gmail.com>
2024-07-01 21:18:58 +0530

    create GenreMovieList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
 import ProductionHouse from './Components/ProductionHouse'
+import GenreMovieList from './Components/GenreMovieList'
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="">
       <Header/>
       <Slider/>
       <ProductionHouse/>
+      <GenreMovieList/>
     </div>
   )
 }
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 
-const movieBaseUrl = "https://api.themoviedb.org/3";
-const api_key = '86959f2a15f1cb25613478d095d0b45f';
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key='2ec0d66f5bdf1dd12eefa0723f1479cf'
 
-const getTrendingVideos = axios.get(`${movieBaseUrl}/trending/all/day`, {
-  params: {
-    api_key: api_key
-  }
-});
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
 
-export default {
-  getTrendingVideos
-};
+
+const getTrendingVideos=axios.get(movieBaseUrl+
+    "/trending/all/day?api_key="+api_key);
+    const getMovieByGenreId=(id)=>
+    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+
+export default{
+    getTrendingVideos,
+    getMovieByGenreId
+}
